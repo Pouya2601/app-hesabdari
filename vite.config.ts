31097ba16784a0b2a1a9aaa1d3ef282d2a4bd628
Pouya2601 <mohammadpooya80@gmail.com>
2026-09-14 00:19:31 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+base: '/app-hesabdari/',
+plugins: [react()],
+resolve: {
+alias: {
+'@': fileURLToPath(new URL('./src', import.meta.url)),
+},
+},
+optimizeDeps: {
+exclude: ['lucide-react'],
+},
 });
